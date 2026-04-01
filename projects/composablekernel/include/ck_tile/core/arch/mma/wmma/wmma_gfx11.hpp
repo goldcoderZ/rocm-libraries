2abc2437dd0b22cf0f801616c71f2c055b6471b3
kiefer <kiefer.van.teutem@streamhpc.com>
2026-04-01 10:34:33 +0000

    Add the remaining two dense wmma intrinsics with packed data types (3 total).

--- projects/composablekernel/include/ck_tile/core/arch/mma/wmma/wmma_gfx11.hpp
+++ projects/composablekernel/include/ck_tile/core/arch/mma/wmma/wmma_gfx11.hpp
@@ -83,4 +83,33 @@ struct amdgcn_mma<fp16_t, fp16_t, fp32_t, 16u, 16u, 16u, CtrlFlags, CompilerTarg
     }
 };
 
+/**
+ * @struct amdgcn_mma
+ * @brief Specialization of amdgcn_wmma for pk_int4_t, pk_int4_t, int32_t MMA operation on GFX11
+ * architecture.
+ * @tparam CtrlFlags Control flags for the WMMA operation
+ * @tparam CompilerTarget Current compiler target
+ */
+// TODO: c++20 template <CtrlFlagsGfx11I CtrlFlags, amdgcn_target CompilerTarget>
+// TODO: c++20 requires
+template <typename CtrlFlags, typename CompilerTarget>
+// clang-format off
+//               | A B C DataTypes             | MNK + WaveSize    |AParams  |BPar |CPar |
+struct amdgcn_mma<pk_int4_t, pk_int4_t, int32_t, 16u, 16u, 16u, CtrlFlags, CompilerTarget, MmaOpFamily::DENSE, enable_if_target_family_gfx11_t<CompilerTarget>>
+: amdgcn_mma_base<pk_int4_t, pk_int4_t, int32_t, 16u, 16u, 16u, 32u, 16, 1, 2, 1, 2, 8, 8, WmmaOp, MmaOpFamily::DENSE>
+// clang-format on
+{
+    CK_TILE_DEVICE static auto
+    exec(AVecType const& aVec, BVecType const& bVec, CVecType const& cVec) -> CVecType
+    {
+        return {__builtin_amdgcn_wmma_i32_16x16x16_iu4_w32(
+            true, // A signedness
+            bit_cast<int32x2_t>(aVec),
+            true, // B signedness
+            bit_cast<int32x2_t>(bVec),
+            cVec,
+            false)}; // TODO: use CtrlFlags for clamp val.
+    }
+};
+
 } // namespace ck_tile::core::arch::mma
