68aba6b787863c9aa617fa3b2cd0a29c64208d01
Randy J. Spaulding <rspauldi@amd.com>
2026-04-12 08:04:17 +0000

    unique sha

--- projects/miopen/src/hip/handlehip.cpp
+++ projects/miopen/src/hip/handlehip.cpp
@@ -196,6 +196,7 @@ MIOPEN_INTERNALS_EXPORT int get_device_id() // Get random device
     return device;
 }
 
+
 // MIOPEN_INTERNALS_EXPORT set here because the function isn't present in headers
 // but called from test/gtest/handle_hip_device.cpp
 MIOPEN_INTERNALS_EXPORT void set_device(int id)
